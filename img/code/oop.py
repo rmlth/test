fe5b6ece023bba2b6e118e8dd571697ccc01f70a
rmlth <2284268273@qq.com>
2024-04-18 13:08:13 +0800

    docs📝: 完善面向对象的笔记

---
+++ img/code/oop.py
@@ -0,0 +1,9 @@
+class CuteCat :
+    def __init__(self, cat_name, cat_age, cat_color ) :
+        self.name = cat_name
+        self.age = cat_age
+        self.color = cat_color
+cat1 = CuteCat ("jj", 2, "red")
+print(f"小猫{cat1.name}的年龄是{cat1.age},它的毛发是{cat1.color}")
+          
+        
